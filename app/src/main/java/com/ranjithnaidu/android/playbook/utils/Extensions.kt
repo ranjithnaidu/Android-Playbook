21a54f8082738a8ab71e4bbb6cdf038c552c4172
Ranjith Naidu <ranjith.naidu@healthengine.com.au>
2019-05-22 12:50:00 +0800

    Main Activity broken to fragment

--- app/src/main/java/com/ranjithnaidu/android/playbook/utils/Extensions.kt
+++ app/src/main/java/com/ranjithnaidu/android/playbook/utils/Extensions.kt
@@ -1,7 +1,18 @@
 package com.ranjithnaidu.android.playbook.utils
 
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 import androidx.lifecycle.MutableLiveData
 
 fun <T> MutableLiveData<T>.setValueIfNew(newValue: T) {
     if (this.value != newValue) value = newValue
+}
+
+/**
+ * Allows calls like
+ *
+ * `supportFragmentManager.inTransaction { add(...) }`
+ */
+inline fun FragmentManager.inTransaction(func: FragmentTransaction.() -> FragmentTransaction) {
+    beginTransaction().func().commit()
 }
